49f3c6bec6218b1e5e510d8f183e777def4a0e1b
Roman <grromf@gmail.com>
2021-12-15 17:21:35 +0800

    added: showing modal by scroll & setTimeout

--- dist/js/script.js
+++ dist/js/script.js
@@ -179,18 +179,21 @@ window.addEventListener('DOMContentLoaded', function () {
   const modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('.modal__close'),
         modalBtns = document.querySelectorAll('[data-modal]');
-  modalBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      modal.classList.add('is-open');
-      document.body.style.overflow = 'hidden';
-    });
-  });
+
+  function openModal() {
+    modal.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+    clearInterval(showModalInterval);
+  }
 
   function closeModal() {
     modal.classList.remove('is-open');
     document.body.style.overflow = '';
   }
 
+  modalBtns.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
   modalCloseBtn.addEventListener('click', closeModal);
   document.addEventListener('keydown', e => {
     if (e.code === 'Escape' && modal.classList.contains('is-open')) {
@@ -202,6 +205,16 @@ window.addEventListener('DOMContentLoaded', function () {
       closeModal();
     }
   });
+  const showModalInterval = setTimeout(openModal, 2000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -105,17 +105,20 @@ window.addEventListener('DOMContentLoaded', function () {
     modalCloseBtn = document.querySelector('.modal__close'),
     modalBtns = document.querySelectorAll('[data-modal]');
 
-  modalBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      modal.classList.add('is-open');
-      document.body.style.overflow = 'hidden';
-    });
-  });
+  function openModal() {
+    modal.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+    clearInterval(showModalInterval);
+  }
 
   function closeModal() {
     modal.classList.remove('is-open');
     document.body.style.overflow = '';
   }
+
+  modalBtns.forEach((btn) => {
+    btn.addEventListener('click', openModal);
+  });
 
   modalCloseBtn.addEventListener('click', closeModal);
 
@@ -130,5 +133,15 @@ window.addEventListener('DOMContentLoaded', function () {
       closeModal();
     }
   });
+
+  const showModalInterval = setTimeout(openModal, 2000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+  window.addEventListener('scroll', showModalByScroll);
 
 });
